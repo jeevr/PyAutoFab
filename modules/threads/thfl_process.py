2718712a3466b5708e373069876a10f0c03fe7dc
jeevr <108726501+jeevr@users.noreply.github.com>
2022-07-08 17:02:56 +0800

    Add files via upload

--- modules/threads/thfl_process.py
+++ modules/threads/thfl_process.py
@@ -1,6 +1,6 @@
 
 # from modules.transform import transform_data
-from modules.transform.transform_data import RawData
+from modules.transform.transform_data import RawData, Checker
 from modules.file_manager.manage import FileManager
 
 # from modules.transform.transform_data import *
@@ -13,6 +13,7 @@ class THFLProcess():
         self.ship = ''
         self.space = ''
         self.input_raw_file_path = ''
+        self.checker_return_errors = []
 
         self._get_ship_details() # get the dropped file details from text file
 
@@ -36,10 +37,28 @@ class THFLProcess():
         manage.copy_raw_input_data(self.input_raw_file_path)
 
     def _load_and_transform(self):
-        self.input_raw_file_path = 'data/_raw_data_input_copy/SC890_CW_ER.txt'
+        self.input_raw_file_path = 'data/_raw_data_input_copy/SC405_CW_ER.txt'
         raw = RawData(self.input_raw_file_path)
         print(raw.df_raw_tray)
         print(raw.df_raw_hanger)
+
+        raw.df_raw_tray.to_excel('df_raw_tray.xlsx')
+        raw.df_raw_hanger.to_excel('df_raw_hanger.xlsx')
+
+        self._apply_checker(raw.df_raw_tray)
+
+    def _apply_checker(self, df_tray):
+        # apply checker
+        checker = Checker()
+
+        # check for rows with null values
+        df_tray_with_nulls = checker.check_rows_with_nulls(df_tray)
+        if len(df_tray_with_nulls) > 0:
+            self.checker_return_errors.append('TRAY WITHH NULLS')
+            df_tray_with_nulls.to_excel('df_tray_with_nulls.xlsx')
+        
+        df_tray_checked = checker.check_tray(df_tray)
+        df_tray_checked.to_excel('df_tray_checked.xlsx')
 
 
 # if __name__ == '__main__': # this code will not be executed when called from other script as a class object
--- modules/transform/transform_data.py
+++ modules/transform/transform_data.py
@@ -1,3 +1,6 @@
+from logging import exception
+from tkinter.messagebox import RETRY
+import numpy as np
 import pandas as pd
 
 class RawData:
@@ -32,10 +35,20 @@ class RawData:
             df_raw_data = pd.read_csv(self.raw_data_file_path, sep='\t')
             df_raw_data['SELECTION'] = df_raw_data.apply(lambda x: self._apply_create_selection_tag(x['HANDLE']), axis=1)
             df_raw_data['ZOOM'] = df_raw_data.apply(lambda x: self._apply_create_zoom_tag(x['HANDLE']), axis=1)
+            df_raw_data.fillna('', inplace=True)
             self.df_raw_data = df_raw_data
             return self.df_raw_data
         except:
             self.return_error = 'INVALID INPUT CONTENTS'
+
+    def _replace_nulls(self, df):
+        df = df.fillna(None)
+        col_heads = list(df.columns)
+        
+        for col in col_heads:
+            df[col] = df[col].str.replace('<>', '')
+        
+        return df
 
     def _check_raw_data_headers(self, df_raw, preset_headers):
         df_raw_headers = list(df_raw.columns)
@@ -81,7 +94,7 @@ class RawData:
         # print(self.df_raw_hanger)
 
         # filter only hangers
-        df_raw_hanger = df_raw_hanger[df_raw_hanger['BLOCKNAME'].isin(self.hanger_columns)]
+        df_raw_hanger = df_raw_hanger[df_raw_hanger['BLOCKNAME'].isin(self.hanger_blocknames)]
         df_raw_hanger = df_raw_hanger.sort_values(by=['BLOCK', 'NO'], ascending=True)
         
         # add missing columns
@@ -95,37 +108,169 @@ class RawData:
 
         self.df_raw_hanger = df_raw_hanger
         # print(self.df_raw_hanger)
-    
+
     def _check_raw_data_per_column(self):
         pass
     
     def _apply_create_selection_tag(self, handle_str):
-        selection_str = f'SELECT (HANDENT"{handle_str[1:]}")  '
-        print(selection_str)
+        selection_str = f'SELECT (HANDENT"{handle_str[1:]}")'
+        # print(selection_str)
         return selection_str
 
     def _apply_create_zoom_tag(self, handle_str):
         selection_str = f'ZOOM OBJECT (HANDENT"{handle_str[1:]}")  '
-        print(selection_str)
+        # print(selection_str)
         return selection_str
 
 
 class Checker:
     def __init__(self):
         pass
+    
+    def check_rows_with_nulls(self, df):
+
+        return df[df.isna().any(axis=1)]
 
     def check_tray(self, df_tray):
         if len(df_tray) == 0:
             return df_tray
         else:
-            pass
+            # df_tray['BLOCK'] = df_tray['BLOCK'].astype('string')
             # check all columns
+            df_tray['BLOCK_ERROR'] = df_tray.apply(lambda x: self._apply_check_block(x['BLOCK']), axis=1)
+            df_tray['NO_ERROR'] = df_tray.apply(lambda x: self._apply_check_no(x['NO'], 'TRAY'), axis=1)
+            df_tray['TRAY_SIZE_ERROR'] = df_tray.apply(lambda x: self._apply_check_tray_size(x['TRAY_SIZE']), axis=1)
+            df_tray['LUGS_ERROR'] = df_tray.apply(lambda x: self._apply_check_tray_lugs(x['NO_OF_LUGS']), axis=1)
+            df_tray['TRO_ANGLE_ERROR'] = df_tray.apply(lambda x: self._apply_check_tro_angle(x['BLOCKNAME'], x['TRO_ANGLE']), axis=1)
+            df_tray['AT_SITE_ERROR'] = df_tray.apply(lambda x: self._apply_check_atsite(x['AT_SITE']), axis=1)
 
-    def _apply_check_tray_block(self, tray_no_of_lugs):
-        pass
 
+            self._mark_tray_rows_with_errors(df_tray)            
+            return df_tray
+    
+    def _mark_tray_rows_with_errors(self, df):
+        # df['ROW_WITH_ERROR'] = df[df['BLOCK_ERROR']=='X' or df['NO_ERROR']=='X']
+        df.loc[
+                (df['BLOCK_ERROR'] == 'X') | 
+                (df['NO_ERROR'] == 'X') | 
+                (df['TRAY_SIZE_ERROR'] == 'X') | 
+                (df['LUGS_ERROR'] == 'X') | 
+                (df['TRO_ANGLE_ERROR'] == 'X') | 
+                (df['AT_SITE_ERROR'] == 'X'),
+                'ROW_WITH_ERROR'] = 'X'
+
+        
+    def _apply_check_block(self, _block):
+        if _block is None:
+            return 'X'
+        if _block == '':
+            return 'X'
+
+    def _apply_check_no(self, _no, _hanger_or_tray):
+        if _no is None:
+            return 'X'
+        if _no == '':
+            return 'X'
+        if _hanger_or_tray == 'TRAY':
+            if len(_no.strip()) != 1:
+                return 'X'
+        if _hanger_or_tray == 'HANGER':
+            if len(_no.strip()) != 2:
+                return 'X'
+
+    def _apply_check_tray_size(self, tray_size):
+        try:
+            ret_val = ''
+            tray_size = tray_size.strip().upper()
+            print(tray_size)
+            if tray_size is None:
+                return 'X'
+            if tray_size == '':
+                return 'X'
+
+            # slice, sample: To-50-19.5, TRo-25
+            _tray = tray_size.split('-')
+            
+            # SEPARATE TRO & TO
+            if len(_tray) == 2: # means TRO
+                if _tray[0] != 'TRO': ret_val = 'X'
+                if len(_tray[1]) != 2: ret_val = 'X'
+            elif len(_tray) == 3: # means TO
+                if _tray[0] != 'TO': ret_val = 'X'
+                if (float(_tray[2])*100) > 2440: ret_val = 'X' # means tray lenth is greater that 2440
+            else:
+                ret_val = ''
+            return ret_val
+        except:
+            return 'X'
+
+    def _apply_check_tray_lugs(self, lugs):
+        try:
+            ret_val = ''
+            if lugs == '<>': # means null
+                pass
+            elif int(lugs) in [0, 1, 2]:
+                pass
+            else:
+                ret_val = 'X'
+            return ret_val
+        except:
+            return 'X'
+
+    def _apply_check_tro_angle(self, block_name, tro_angle):
+        try:
+            ret_val = ''
+            
+            if block_name == 'ELECT_CWAY_TRAYBLOCK_A': # means null
+                if tro_angle == '<>' or tro_angle == '':
+                    pass
+            elif len(tro_angle) == 2 and block_name == 'ELECT_CWAY_TRAY_BLOCK_TRO':
+                pass
+            else:
+                ret_val = 'X'
+            return ret_val
+        except:
+            return 'X'
+
+    def _apply_check_atsite(self, atsite):
+        try:
+            ret_val = ''
+            if atsite.strip() in ['NO', 'YES']:
+                pass
+            else:
+                ret_val = 'X'
+            return ret_val
+        except:
+            return 'X'
+            
     def check_hanger(self, df_hanger):
         if len(df_hanger) == 0:
             return df_hanger
         else:
-            pass
+            df_hanger['BLOCK_ERROR'] = df_hanger.apply(lambda x: self._apply_check_block(x['BLOCK']), axis=1)
+            df_hanger['NO_ERROR'] = df_hanger.apply(lambda x: self._apply_check_no(x['NO'], 'HANGER'), axis=1)
+            df_hanger['HANGER_SIZE_ERROR'] = df_hanger.apply(lambda x: self._apply_check_hanger_size(x['HANGER_SIZE']), axis=1)
+            df_hanger['AT_SITE_ERROR'] = df_hanger.apply(lambda x: self._apply_check_atsite(x['AT_SITE']), axis=1)
+
+            self._mark_hanger_rows_with_errors(df_hanger)            
+            return df_hanger
+
+    def _mark_hanger_rows_with_errors(self, df):
+       
+        df.loc[
+                (df['BLOCK_ERROR'] == 'X') | 
+                (df['NO_ERROR'] == 'X') | 
+                (df['AT_SITE_ERROR'] == 'X'),
+                'ROW_WITH_ERROR'] = 'X'
+
+    def _apply_check_hanger_size(self, hanger_size):
+        try:
+            hanger_size = hanger_size.upper()
+            ret_val = ''
+            if (hanger_size.find('A') != -1) or (hanger_size.find('AX2') != -1):
+                pass # means the string is found
+            else:
+                ret_val = 'X'
+            return ret_val
+        except:
+            return ''
